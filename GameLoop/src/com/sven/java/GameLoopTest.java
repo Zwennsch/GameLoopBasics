991b05b4a435a27d515ce1d4efb480bb11063952
svens <schuen@gmx.de>
2019-06-19 15:41:19 +0200

    added gitignore file

--- GameLoop/src/com/sven/java/GameLoopTest.java
+++ GameLoop/src/com/sven/java/GameLoopTest.java
@@ -1,10 +1,21 @@
 package com.sven.java;
 
-public class GameLoopTest {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class GameLoopTest extends JFrame implements ActionListener{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
